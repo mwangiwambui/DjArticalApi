66454de326f33f9de746e393d34febdb66bdadde
mwangiwambui <wambui54mwangi@gmail.com>
2020-03-30 23:16:54 +0300

    Added the config files

---
+++ article/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Article
+# Create your views here.
+class ArticleView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        return Response({"articles": articles})
